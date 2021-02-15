c15423bb5a0b8bf05722b0787149c322b992febc
Diego Bufalo <diego.bufalo14@gmail.com>
2021-02-15 01:13:59 -0300

    ROLE, PROFILE, DOCKER
    Adicionado a autenticação com ROLE, separado os perfis de teste,
    desenvolvimento e produção, e configurado o perfil de deploy com docker.

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,4 +1,11 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$t2fbbgKBY0tcdHwwgqovD.ehJy79yFH6gZ8gDUN0JEb86zk26cREK');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$t2fbbgKBY0tcdHwwgqovD.ehJy79yFH6gZ8gDUN0JEb86zk26cREK');
+
+INSERT INTO PERFIL(id, nome)  VALUES(1, 'ROLE_ALUNO');
+INSERT INTO PERFIL(id, nome)  VALUES(2, 'ROLE_MODERADOR');
+
+INSERT into USUARIO_PERFIS(usuario_id, perfis_id) VALUES (1, 1);
+INSERT into USUARIO_PERFIS(usuario_id, perfis_id) VALUES (2, 2);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
--- src/test/java/br/com/diegobufalo/forum/ForumApplicationTests.java
+++ src/test/java/br/com/diegobufalo/forum/ForumApplicationTests.java
@@ -1,13 +1,19 @@
 package br.com.diegobufalo.forum;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.util.Assert;
 
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ForumApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.isTrue(true);
 	}
 
 }
